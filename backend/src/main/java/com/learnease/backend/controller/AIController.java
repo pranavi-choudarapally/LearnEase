6b8e63cece02ec71589a812778d12616b0e4608a
Pranavi Choudarapally <pranavichoudarapally@gmail.com>
2026-08-03 03:55:20 +0530

    fixed cross origin settings in AIController to allow production urls

--- backend/src/main/java/com/learnease/backend/controller/AIController.java
+++ backend/src/main/java/com/learnease/backend/controller/AIController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/ai")
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "*", allowedHeaders = "*") 
 public class AIController {
 
     private final OpenRouterService openRouterService;
